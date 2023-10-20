e9398dd9c3eba3277a346e2f1d167974e62ba4e4
GabrielEscamilla07 <147454052+GabrielEscamilla07@users.noreply.github.com>
2023-10-19 20:27:22 -0500

    ORIGINAL

--- Unopqroo.js
+++ Unopqroo.js
@@ -37,8 +37,44 @@ for (var cColor = 0; cColor < colors.length; cColor++ ) {
  deck.push({color: null, number: "CC", type: "Comodin"});
  deck.push({color: null, number: "CC", type: "Comodin"});
 
- console.log(deck);
+// console.log(deck);
 
 const players = rl.question("cuantos jugadores seran?", {})
+const totalCards = players * 7;
+//reparitir 7 cartas x jugador
+//recorro el totaal de tarjetas (total de tarjetas = trajetas/7 * jugador)
+//un arregolo * jugador
+var cardsPlayers = {};
 
-console.log(players);
+for(var cTCards = 0; cTCards < 7; cTCards++){
+    for(var cPlayers = 0; cPlayers < players; cPlayers++){
+       if (!cardsPlayers["player_"+cPlayers]){
+        cardsPlayers["player_"+cPlayers] = [];
+       }
+       
+        cardsPlayers["player_"+cPlayers].push(deck.shift());
+    }
+}
+
+var trash = [];
+trash.push (deck.shift());
+
+console.log(trash);
+ 
+function cardValidation (card) {
+    console.log(card.number);
+    if (card.color == trash[trash.length-1].color || card.number == trash[trash.length-1].number) {
+        console.log("es igual");    
+    } else {
+        console.log("no es igual");
+    }
+}
+//console.log();
+//cardValidation({color: "yellow",number: 2,type: "comun"});
+
+//for(const player in cardsPlayers){}
+for (var MoreFour = 0; MoreFour < 4; MoreFour++) {
+    cardsPlayers["player_1"].push(deck.shift());
+}
+console.log("jugador actual", cardsPlayers["player_0"]);
+console.log("siguiente jugador", cardsPlayers["player_1"]);
